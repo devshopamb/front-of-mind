88993490af8d735a4170c45b41cd8c73ed7b70cf
devshopamb <akmbrown@gmail.com>
2025-12-02 08:43:09 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-```javascript
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -9,4 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-```
